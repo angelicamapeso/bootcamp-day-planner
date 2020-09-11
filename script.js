98cd020e2b7e7582eaf7216fae5636d654a429f5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-11 13:45:03 -0400

    Add javascript file

---
+++ script.js
@@ -0,0 +1,20 @@
+//put the current day in the currentDay id place
+  //current day formatter
+
+//dynamically generate:
+  //rows of container
+  //3 columns:
+    //hours from 9-5
+    //text area
+    //save btn
+
+//check the current time
+  //colour based on if it's the current time
+    //or not
+
+
+//to get the current hour
+console.log(moment().format('H'));
+
+//to get the current day, formatted as..
+console.log(moment().format('dddd, MMMM Do'));
